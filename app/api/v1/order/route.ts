e2adebf8888001282e205c19c1df764c8d7f8944
Muhammad Ranju <mdranju23@gmail.com>
2026-05-12 14:50:46 +0600

    feat: implement v1 API routes for products, reviews, users, and orders with role-based authentication

---
+++ app/api/v1/order/route.ts
@@ -0,0 +1,6 @@
+import { OrderController } from '@/lib/server/app/modules/order/order.controller';
+import auth from '@/lib/server/app/middlewares/auth';
+import { USER_ROLES } from '@/lib/server/enums/user';
+
+export const POST = OrderController.createOrder;
+export const GET = auth(USER_ROLES.SUPER_ADMIN, USER_ROLES.ADMIN)(OrderController.getAllOrders);
